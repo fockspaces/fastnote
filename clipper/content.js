1217c89b45768468f57b632bd858402efb736ad3
Fock <fock@FockdeMacBook-Air.local>
2023-04-02 15:37:18 +0800

    [clipper] setup react app

--- clipper/content.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  // Check that the message type is 'clipText'
-  if (message.type === "clipText") {
-    // Get the selected text on the page
-    const selectedText = window.getSelection().toString();
-    // Send the selected text back to the background script
-    sendResponse(selectedText);
-  }
-});
