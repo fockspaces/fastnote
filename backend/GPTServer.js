180422b601c2b30bdc6bafdba085e602afb06ea6
Fock <fock@FockdeMacBook-Air.local>
2023-05-04 10:51:02 +0800

    [GPTServer] add rate limiting for GPT server

--- backend/GPTServer.js
+++ backend/GPTServer.js
@@ -1,25 +1,26 @@
 import * as dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import errorHandler from "express-error-handler";
 import { verifyUser } from "./middleware/Authentication.js";
 import { fetchGPT } from "./utils/fetchGPT.js";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 const app = express();
-// connect to db
-const { MONGODB_URI } = process.env;
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
+// Rate limiter configuration
+const rateLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 3, // limit to 10 requests per windowMs
+  message: "Too many requests, please try again later.",
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(verifyUser);
 
-app.post("/api/summary", async (req, res) => {
+app.post("/api/summary", rateLimiter, async (req, res) => {
   const { prompt } = req.body;
   const data = await fetchGPT(prompt);
   console.log({ data });
