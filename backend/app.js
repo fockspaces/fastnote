950eff5556a352db13d21d4cb15a59026ddc6e7a
Fock <fock@FockdeMacBook-Air.local>
2023-05-11 17:19:58 +0800

    [backend] feat: update CORS configuration in app.js - Replace general CORS middleware with a custom one.

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,13 @@ mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:3000", process.env.CLIENT_HOST],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(createRateLimiter);
 
